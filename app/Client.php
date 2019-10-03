163ba0c3a5c589a3a30bf292ed136d36c4d8aabb
jose eduardo lopes de souza <jos3duardolopes@gmail.com>
2019-10-02 22:58:11 -0400

    up project with category, seeder and factory configured

---
+++ app/Client.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    const MARITAL_STATUS = [
+        1 => 'Solteiro',
+        2 => 'Casado',
+        3 => 'Divorciado'
+    ];
+}
---
+++ database/factories/ClientFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use Faker\Generator as Faker;
+
+require_once __DIR__.'/../faker_data/document_number.php';
+
+$factory->define(App\Client::class, function (Faker $faker) {
+    $cpfs = cpfs();
+    return [
+        'name' => $faker->name,
+        'document_number' => $cpfs[array_rand($cpfs,1)],
+        'email' => $faker->email,
+        'phone' => $faker->phoneNumber,
+        'defaulter' => rand(0,1),
+        'date_birth'=> $faker->date(),
+        'sex' => rand(1,10) % 2 == 0 ? 'm' : 'f',
+        'marital_status' => rand(1,3),
+        'physical_disability' => rand(1,10) % 2 == 0 ? $faker->word : null
+    ];
+});
